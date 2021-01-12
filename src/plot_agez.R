c2432250a9c071514a7baaff616a5e7e960fb605
Daniele Silvestro <silvestro.daniele@gmail.com>
2021-01-12 22:49:46 +0100

    Update plot_agez.R

--- src/plot_agez.R
+++ src/plot_agez.R
@@ -12,7 +12,7 @@ tbl = read.csv(f)
 # PLOT ZIRCON RAW DATA AND ESTIMATED AGE
 plot_data <-function(m, s, indx=c(1:10), xlim=c(9, 20)){
 	plot(NA, xlim=xlim, ylim=c(0,1), xlab="Ma", ylab="Relative density",
-	     main="Sampled zironc ages")
+	     main="Sampled zircon ages")
 	for (i in indx){
 		vec = seq(m[i]-2*s[i], m[i]+2*s[i], length.out=100)
 		Ys = dnorm(vec, m[i], s[i])
@@ -31,17 +31,24 @@ plot_data(c(10, 14, 17), c(0.2, 0.4, 1), indx=c(1, 2,3)) #, xlim=c(0,100))
 
 
 # PLOT CAUCHY
-plot_sample <- function(samples,xlim=c(0,20)){
+plot_sample <- function(samples,xlim=c(0,20), UniCau=FALSE, p_error=0.1){
 	plot(NA, xlim=xlim, ylim=c(0,length(samples)), xlab="Ma", ylab="Samples",
-	     main="")
+	     main="Estimated sample ages")
 	for (i in 1:length(samples)){
 		samp = samples[[i]]
 		vec = seq(min(xlim), max(xlim), length.out=1000)
 		Ys = dcauchy(vec, samp$x, samp$s)
+		if (UniCau){
+			Ys[vec < samp$x] = 1/(samp$x / 2)
+			# include P(I)
+			Ys[vec < samp$x] = p_error * Ys[vec < samp$x]
+			Ys[vec > samp$x] = (1 - p_error) * Ys[vec > samp$x]
+		}
 		Ys = Ys - min(Ys)
-		Ys = 0.9 * (Ys / max(Ys)) + (i-1)
-		lines(vec, Ys)
-		segments(samp$x,min(Ys),samp$x,max(Ys), col="gray50",lty=2) # mean
+		Ys = 0.8 * (Ys / max(Ys)) + (i-1)
+		lines(vec, Ys, lwd=2, col='black')
+		abline(h=min(Ys), lty=2)
+		# segments(samp$x,min(Ys),samp$x,max(Ys), col="gray50",lty=2) # mean
 		points(samp$z[samp$I == 1], rep(min(Ys), sum(samp$I)), col="red",pch=21, bg="red") # true
 		points(samp$z[samp$I == 0], rep(min(Ys), length(samp$I)-sum(samp$I)),
 			col="red", bg="white",pch=21) # wrong
@@ -49,6 +56,8 @@ plot_sample <- function(samples,xlim=c(0,20)){
 		
 	}
 }
+
+
 
 
 sample_1 = NULL
@@ -65,11 +74,11 @@ sample_2$I = c(1, 1, 1)
 
 sample_3 = NULL
 sample_3$x = 13
-sample_3$s = 3
+sample_3$s = 5
 sample_3$z = c(10, 13.8, 17)
 sample_3$I = c(0, 1, 1)
 
 
 samples=list(sample_1, sample_2, sample_3)
 
-plot_sample(samples, xlim=c(5,25))
+plot_sample(samples, xlim=c(5,25), UniCau=T, p_error=0.1)
